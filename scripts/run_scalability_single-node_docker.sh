3ab2ee580bfaff8fe4628f99da856407456a7aec
Fernando Cuesta <fernandocuestab@gmail.com>
2025-08-12 19:45:00 +0200

    fix: include 'nodes' in CSV headers and log results for scalability tests

--- scripts/run_scalability_single-node_docker.sh
+++ scripts/run_scalability_single-node_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+NODES=1
 THREADS_LIST=(1 2 4 8 16)
 
 CONFIG="docker-examples/ubuntu-no-gpu/Hpmoon/config.xml"
@@ -9,29 +10,38 @@ WORKDIR="docker-examples/ubuntu-no-gpu/Hpmoon"
 LOGDIR="logs"
 IMAGE="hpmoon-ubuntu-no-gpu:v0.0.5"
 
-echo "threads,time,max_memory,cpu_percentage" > $RESULTS
+echo "nodes,threads,time,max_memory,cpu_percentage" > $RESULTS
 
 for THREADS in "${THREADS_LIST[@]}"
 do
     echo "------------------------------------------------------------"
     echo "Starting test with $THREADS threads (Docker)..."
+
+    # Clean the system before running the test
     ./scripts/clean_system.sh
 
+    # Change the number of threads in the configuration file
     echo "Updating <CpuThreads> to $THREADS in $CONFIG"
     sed -i "s/<CpuThreads>[0-9]\+<\/CpuThreads>/<CpuThreads>${THREADS}<\/CpuThreads>/" "$CONFIG"
 
+    # Change the logfile name to include the number of threads
     LOGFILE="$LOGDIR/mononode_docker_${THREADS}threads.log"
 
+    # Run the program in Docker and save the log
     echo "Running the program in Docker and saving log to $LOGFILE"
     /usr/bin/time -v docker run --rm \
     -v "$PWD/$CONFIG":/root/Hpmoon/config.xml \
     $IMAGE > "$LOGFILE" 2>&1
 
+    # Extract metrics from the log file
     echo "Extracting metrics from $LOGFILE"
     real_time=$(grep "Elapsed (wall clock) time" "$LOGFILE" | awk '{print $8}')
     max_memory=$(grep "Maximum resident set size" "$LOGFILE" | awk '{print $6}')
     cpu_percentage=$(grep "Percent of CPU this job got" "$LOGFILE" | awk -F: '{gsub(/%/,""); print $2}' | xargs)
-    echo "$THREADS,$real_time,$max_memory,$cpu_percentage" >> $RESULTS
+
+    # Log the results
+    echo "$NODES,$THREADS,$real_time,$max_memory,$cpu_percentage" >> $RESULTS
+
     echo "Test with $THREADS threads (Docker) finished."
 done
 
--- scripts/run_scalability_single-node_native.sh
+++ scripts/run_scalability_single-node_native.sh
@@ -2,6 +2,7 @@
 
 # scripts/run_single-node.sh
 
+NODES=1
 THREADS_LIST=(1 2 4 8 16)
 
 CONFIG="docker-examples/ubuntu-no-gpu/Hpmoon/config.xml"
@@ -11,32 +12,41 @@ WORKDIR="docker-examples/ubuntu-no-gpu/Hpmoon"
 LOGDIR="logs"
 
 # CSV header
-echo "threads,time,max_memory,cpu_percentage" > $RESULTS
+echo "nodes,threads,time,max_memory,cpu_percentage" > $RESULTS
 
 for THREADS in "${THREADS_LIST[@]}"
 do
     echo "------------------------------------------------------------"
     echo "Starting test with $THREADS threads..."
     echo "Running system cleanup..."
+
+    # Clean the system before running the test
     ./scripts/clean_system.sh
 
+    # Change the number of threads in the configuration file
     echo "Updating <CpuThreads> to $THREADS in $CONFIG"
     sed -i "s/<CpuThreads>[0-9]\+<\/CpuThreads>/<CpuThreads>${THREADS}<\/CpuThreads>/" "$CONFIG"
 
+    # Change the logfile name to include the number of threads
     LOGFILE="$LOGDIR/single-node_${THREADS}threads.log"
     LOGFILE_RELATIVE="$(realpath --relative-to="$WORKDIR" "$LOGFILE")"
 
+    # Run the program
     echo "Running the program and saving log to $LOGFILE"
     (
         cd "$WORKDIR"
         /usr/bin/time -v mpirun --bind-to none --allow-run-as-root --map-by node --host localhost ./$EXEC -conf config.xml -ns $THREADS > "$LOGFILE_RELATIVE" 2>&1
     )
 
+    # Extract metrics from the log file
     echo "Extracting metrics from $LOGFILE"
     real_time=$(grep "Elapsed (wall clock) time" "$LOGFILE" | awk '{print $8}')
     max_memory=$(grep "Maximum resident set size" "$LOGFILE" | awk '{print $6}')
     cpu_percentage=$(grep "Percent of CPU this job got" "$LOGFILE" | awk -F: '{gsub(/%/,""); print $2}' | xargs)
-    echo "$THREADS,$real_time,$max_memory,$cpu_percentage" >> $RESULTS
+
+    # Log the results
+    echo "$NODES,$THREADS,$real_time,$max_memory,$cpu_percentage" >> $RESULTS
+    
     echo "Test with $THREADS threads finished."
 done
 
