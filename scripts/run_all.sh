aaff06bc04ef0ce25fa59066ccb258da697bbd33
Fernando Cuesta Bueno <fernandocuestab@gmail.com>
2025-08-20 23:33:48 +0200

    feat: update run_all.sh for no-GPU configuration and adjust parameter order

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -4,24 +4,25 @@
 # ./scripts/run_single-node.sh
 
 # Configuration
-BASE_DIR="docker-examples/ubuntu-gpu"
+BASE_DIR="docker-examples/ubuntu-no-gpu"
 WORKDIR="$BASE_DIR/Hpmoon"
 RESULTS_DIR="results"
 EXEC="bin/hpmoon"
 LOGDIR="logs"
 
 # Parameters
-PARAMS="$BASE_DIR $RESULTS_DIR $EXEC $WORKDIR $LOGDIR"
+PARAMS="$BASE_DIR $WORKDIR $RESULTS_DIR $EXEC $LOGDIR"
 
 # Script list
 SCRIPTS=(
-    "./scripts/single-node/run_single-node_native.sh"
-    "./scripts/single-node/run_single-node_container.sh"
-    "./scripts/multi-node/run_multi-node_native.sh"
-    "./scripts/multi-node/run_multi-node_container.sh"
-    "./scripts/multi-node/run_sweep-threads_native.sh"
-    "./scripts/multi-node/run_sweep-threads_container.sh"
-    "./scripts/multi-node/run_sweep-threads_no-limit_native.sh"
+    "./scripts/single-node/run_ubuntu_native.sh"
+    # "./scripts/single-node/run_ubuntu_container.sh"
+    # "./scripts/single-node/run_cluster_container.sh"
+    # "./scripts/multi-node/run_ubuntu_native.sh"
+    # "./scripts/multi-node/run_ubuntu_container.sh"
+    # "./scripts/sweep-threads/run_ubuntu_native.sh"
+    # "./scripts/sweep-threads/run_ubuntu_container.sh"
+    # "./scripts/sweep-threads/run_no-limit_ubuntu_native.sh"
 )
 
 for script in "${SCRIPTS[@]}"; do
