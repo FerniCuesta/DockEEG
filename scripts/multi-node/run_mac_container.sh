16bab312f0e498909d3bd3fb64b44f7ee5fb0bcf
Fernando Cuesta Bueno <fernandocuestab@gmail.com>
2025-08-28 01:49:21 +0200

    feat: add script for running multi-node tests on macOS with Docker and Podman

---
+++ scripts/multi-node/run_mac_container.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Configuration
+BASE_DIR="${1:-docker-examples/ubuntu-no-gpu}"
+WORKDIR="${2:-$BASE_DIR/Hpmoon}"
+RESULTS_DIR="${3:-results}"
+EXEC="${4:-bin/hpmoon}"
+LOGDIR="${5:-logs}"
+
+CONFIG="$WORKDIR/config.xml"
+IMAGE="ferniicueesta/hpmoon-ubuntu-no-gpu:v0.0.6"
+
+# Test parameters
+CONTAINER_LIST=("docker" "podman")
+NODES_LIST=(1 2 4 8 16)
+THREADS=1
+
+# Create directories if they do not exist
+mkdir -p "$RESULTS_DIR" "$LOGDIR"
+
+for CONTAINER in "${CONTAINER_LIST[@]}"
+do
+    RESULTS="$RESULTS_DIR/multi-node/mac_${CONTAINER}.csv"
+
+    # CSV header
+    echo "nodes,threads,time,max_memory,cpu_percentage" > "$RESULTS"
+
+    for NODES in "${NODES_LIST[@]}"
+    do
+        echo "------------------------------------------------------------"
+        echo "Starting test with $NODES nodes and $THREADS threads ($CONTAINER)..."
+
+        # Change the number of threads in the configuration file (macOS sed is different)
+        echo "Updating <CpuThreads> to $THREADS in $CONFIG"
+        sed -i '' -E "s|<CpuThreads>[[:space:]]*[0-9]+[[:space:]]*</CpuThreads>|<CpuThreads>${THREADS}</CpuThreads>|" "$CONFIG"
+
+        # Build the hosts string
+        HOSTS=$(yes localhost | head -n $NODES | paste -sd, -)
+
+        # Change the logfile name to include the number of threads and container
+        LOGFILE="$LOGDIR/multi-node/mac_${CONTAINER}_${NODES}nodes_${THREADS}threads.log"
+
+        # Run the program in Docker or Podman and save the log
+        echo "Running the program in $CONTAINER and saving log to $LOGFILE"
+        gtime -v $CONTAINER run --rm \
+            -v "$PWD/$CONFIG":/root/Hpmoon/config.xml \
+            -w /root/Hpmoon \
+            $IMAGE \
+            mpirun --bind-to none --allow-run-as-root --map-by node --host $HOSTS ./bin/hpmoon -conf config.xml > "$LOGFILE" 2>&1
+
+        # Extract metrics from the log file
+        echo "Extracting metrics from $LOGFILE"
+        time=$(grep "Elapsed (wall clock) time" "$LOGFILE" | awk '{print $8}')
+        max_memory=$(grep "Maximum resident set size" "$LOGFILE" | awk '{print $6}')
+        cpu_percentage=$(grep "Percent of CPU this job got" "$LOGFILE" | awk -F: '{gsub(/%/,"",$2); print $2}' | xargs)
+
+        # Log the results
+        echo "$NODES,$THREADS,$time,$max_memory,$cpu_percentage" >> "$RESULTS"
+
+        echo "Test with $NODES nodes and $THREADS threads ($CONTAINER) finished."
+    done
+done
+
+echo "------------------------------------------------------------"
+echo "All multinode tests have finished. Results in $RESULTS_DIR"
