c0cd50308afbebccb03b14e4bb7af09f0b07d8e1
Fernando Cuesta Bueno <fernandocuestab@gmail.com>
2025-09-02 21:19:40 +0200

    feat: Adds functionality to upload execution data from container runs on Windows

--- scripts/thread-sweep/run_windows_container.ps1
+++ scripts/thread-sweep/run_windows_container.ps1
@@ -19,8 +19,8 @@ New-Item -ItemType Directory -Path $ResultsDir -Force | Out-Null
 New-Item -ItemType Directory -Path $LogDir -Force | Out-Null
 
 foreach ($Container in $ContainerList) {
-    $Results = "$ResultsDir/thread-sweep/ubuntu_${Container}.csv"
-    "nodes,threads,time,max_memory,cpu_percentage" | Set-Content $Results
+    $Results = "$ResultsDir/thread-sweep/windows_${Container}.csv"
+    "nodes,threads,time" | Set-Content $Results
 
     foreach ($Nodes in $NodesList) {
         foreach ($Threads in $ThreadsList) {
@@ -35,7 +35,7 @@ foreach ($Container in $ContainerList) {
             # Build the hosts string
             $Hosts = [string]::Join(",", (1..$Nodes | ForEach-Object { "localhost" }))
 
-            $LogFile = "$LogDir\thread-sweep\ubuntu_${Container}_${Nodes}nodes_${Threads}threads.log"
+            $LogFile = "$LogDir\thread-sweep\windows_${Container}_${Nodes}nodes_${Threads}threads.log"
             New-Item -ItemType Directory -Path (Split-Path $LogFile) -Force | Out-Null
 
             Write-Host "Running the program in $Container and saving log to $LogFile"
@@ -49,15 +49,18 @@ foreach ($Container in $ContainerList) {
                 "--host", $Hosts,
                 "./bin/hpmoon", "-conf", "config.xml"
             )
+            $startTime = Get-Date
+
             & $Container @dockerArgs *>&1 | Tee-Object -FilePath $LogFile
 
-            # Extract metrics from the log
-            Write-Host "Extracting metrics from $LogFile"
-            $time = Select-String -Path $LogFile -Pattern "Elapsed \(wall clock\) time" | ForEach-Object { $_.Line.Split()[-1] }
-            $max_memory = Select-String -Path $LogFile -Pattern "Maximum resident set size" | ForEach-Object { $_.Line.Split()[-1] }
-            $cpu_percentage = Select-String -Path $LogFile -Pattern "Percent of CPU this job got" | ForEach-Object { $_.Line.Split(":")[-1].Trim().TrimEnd("%") }
+            $endTime = Get-Date
+            $elapsed = $endTime - $startTime
+            $time = $elapsed.TotalSeconds
 
-            "$Nodes,$Threads,$time,$max_memory,$cpu_percentage" | Add-Content $Results
+            # Write metrics to the results file
+            Write-Host "Writing metrics in $Results"
+
+            "$Nodes,$Threads,$time" | Add-Content $Results
 
             Write-Host "Test with $Nodes nodes and $Threads threads ($Container) finished."
         }
